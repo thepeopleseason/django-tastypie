a16a2397f1e14b1b4b72d0e63b5ae8618ca120aa
Daniel Lindsley <daniel@toastdriven.com>
2010-12-21 02:51:59 -0600

    Initial digest implementation. Needs some work & testing.

--- tastypie/authentication.py
+++ tastypie/authentication.py
@@ -1,7 +1,13 @@
 import base64
 import hmac
+import time
 from django.contrib.auth import authenticate
-from http import HttpUnauthorized
+from tastypie.http import HttpUnauthorized
+try:
+    from hashlib import sha1
+except ImportError:
+    import sha
+    sha1 = sha.sha
 
 
 class Authentication(object):
@@ -155,3 +161,110 @@ class ApiKeyAuthentication(Authentication):
         This implementation returns the user's username.
         """
         return request.REQUEST.get('username', 'nouser')
+
+
+class DigestAuthentication(Authentication):
+    """
+    Handles HTTP Digest auth against a specific auth backend if provided,
+    or against all configured authentication backends using the
+    ``authenticate`` method from ``django.contrib.auth``. However, instead of
+    the user's password, their API key should be used.
+    
+    Optional keyword arguments:
+
+    ``backend``
+        If specified, use a specific ``django.contrib.auth`` backend instead
+        of checking all backends specified in the ``AUTHENTICATION_BACKENDS``
+        setting.
+    ``realm``
+        The realm to use in the ``HttpUnauthorized`` response.  Default:
+        ``django-tastypie``.
+    """
+    def __init__(self, backend=None, realm='django-tastypie'):
+        self.backend = backend
+        self.realm = realm
+
+    def _unauthorized(self):
+        import python_digest
+        response = HttpUnauthorized()
+        new_uuid = uuid.uuid4()
+        opaque = hmac.new(str(new_uuid), digestmod=sha1).hexdigest()
+        response['WWW-Authenticate'] = python_digest.build_digest_challenge(time.time(), getattr(settings, 'SECRET_KEY', ''), self.realm, opaque, False)
+        return response
+
+    def is_authenticated(self, request, **kwargs):
+        """
+        Finds the user and checks their API key.
+        
+        Should return either ``True`` if allowed, ``False`` if not or an
+        ``HttpResponse`` if you need something custom.
+        """
+        if not request.META.get('HTTP_AUTHORIZATION'):
+            return self._unauthorized()
+        
+        try:
+            (auth_type, data) = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
+            
+            if auth_type != 'Digest':
+                return self._unauthorized()
+        except:
+            return self._unauthorized()
+        
+        digest_response = python_digest.parse_digest_credentials(request.META['HTTP_AUTHORIZATION'])
+        
+        # FIXME: Should the nonce be per-user? Will this even work?
+        if not python_digest.validate_nonce(digest_response.nonce, secret):
+            return self._unauthorized()
+        
+        user = self.get_user(digest_response.username)
+        api_key = self.get_key(user, api_key)
+        
+        if user is False or api_key is False:
+            return self._unauthorized()
+        
+        expected = python_digest.calculate_request_digest(request.method, digest_response, python_digest.calculate_partial_digest(digest_response.username, self.realm, api_key))
+        
+        if not digest_response.response == expected:
+            return self._unauthorized()
+        
+        request.user = user
+        return True
+    
+    def get_user(self, username):
+        from django.contrib.auth.models import User
+        
+        try:
+            user = User.objects.get(username=username)
+        except (User.DoesNotExist, User.MultipleObjectsReturned):
+            return False
+        
+        return user
+    
+    def get_key(self, user):
+        """
+        Attempts to find the API key for the user. Uses ``ApiKey`` by default
+        but can be overridden.
+        
+        Note that this behaves differently than the ``ApiKeyAuthentication``
+        method of the same name.
+        """
+        from tastypie.models import ApiKey
+        
+        try:
+            key = ApiKey.objects.get(user=user)
+        except ApiKey.DoesNotExist:
+            return False
+        
+        return key
+    
+    def get_identifier(self, request):
+        """
+        Provides a unique string identifier for the requestor.
+        
+        This implementation returns the user's username.
+        """
+        if hasattr(request, 'user'):
+            if hasattr(request.user, 'username'):
+                return request.user.username
+        
+        return 'nouser'
